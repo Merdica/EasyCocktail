020b6f0848455a008a7e312dd940199f609d6dde
Melanie Schmidt <melanie.schmidt@quartett-mobile.de>
2019-06-11 14:02:12 +0200

    navigation architcture works now

--- app/src/main/java/de/twosoulsmedia/easycocktails/CocktailListFragment.kt
+++ app/src/main/java/de/twosoulsmedia/easycocktails/CocktailListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 
@@ -18,5 +19,8 @@ class CocktailListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_cocktailListFragment_to_cocktailDetailFragment)
+        }
     }
 }
